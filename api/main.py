0c9b7545e85a90809bc1591e4787bfe4761769d5
Jose <jose@sirven.xyz>
2024-03-19 22:03:37 -0400

    initial commit

---
+++ api/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, Depends
+from authenticator import authenticator
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
+from routers import (
+    # all the domains
+)
+
+app = FastAPI()
+app.include_router(authenticator.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        os.environ.get("CORS_HOST", "http://localhost:3000"),
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/launch-details")
+def launch_details():
+    return {
+        "launch_details": {
+            "module": 3,
+            "week": 17,
+            "day": 5,
+            "hour": 19,
+            "min": "00",
+        }
+    }
+
+
+# app.include_router(users.router)
+# app.include_router(plans.router)
+# app.include_router(savings.router)
+# app.include_router(transactions.router)
+# app.include_router(journals.router)
+# app.include_router(expenses.router)
+# app.include_router(expenses_type_vo.router)
