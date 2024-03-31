a65aa334072bb2bf24eceb7b0944e63f18830297
Jose <jose@sirven.xyz>
2024-03-31 01:39:15 -0400

    cleaned up homepage

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,4 @@
 import { Metadata } from "next";
-import Image from "next/image";
-import Link from "next/link";
-
-import { UserAuthForm } from "@components/user-auth-form";
 
 export const metadata: Metadata = {
   title: "Authentication",
@@ -12,22 +8,6 @@ export const metadata: Metadata = {
 export default function HomePage() {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/authentication-light.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/authentication-dark.png"
-          width={1280}
-          height={843}
-          alt="Authentication"
-          className="hidden dark:block"
-        />
-      </div>
       <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900" />
@@ -85,36 +65,7 @@ export default function HomePage() {
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8">
-          {/* <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Join hello_miami
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your phone number below to get started
-              </p>
-            </div>
-            <UserAuthForm />
-            <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{" "}
-              <Link
-                href="/terms"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Terms of Service
-              </Link>{" "}
-              and{" "}
-              <Link
-                href="/privacy"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Privacy Policy
-              </Link>
-              .
-            </p>
-          </div> */}
-        </div>
+
         <video
           autoPlay
           loop
