4353103966829ab8de12a80dd471122a33297728
Jose <jose@sirven.xyz>
2024-03-31 01:55:23 -0400

    added survey calendar join button to floating navbar

--- client/components/floating-navbar.tsx
+++ client/components/floating-navbar.tsx
@@ -1,47 +1,65 @@
 "use client";
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
-import Link from 'next/link';
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 const FloatingNavbar = () => {
-
-    return (
-      <motion.div
-        className="hidden sm:flex fixed inset-x-0 bottom-0 w-full p-7 z-100 justify-center items-center rounded-full"
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <div className="flex justify-center items-center border  border-white/50 bg-black backdrop-blur-md rounded-full p-1.5">
-            <div className="flex boder items-stretch p-2">
-                <Link
-                    href="#hero"
-                    style={{
-                      backgroundImage: 'url(https://assets-global.website-files.com/5837424ae11409586f837994/624322b0c806026f5689d841_Group%2012569.svg)',
-                      backgroundPosition: '50%',
-                      backgroundRepeat: 'no-repeat',
-                      backgroundSize: 'auto 22px',
-                      paddingLeft: '38px',
-                      paddingRight: '38px'
-                    }}
-                    className="text-white p-4 font-bold rounded-full bg-zinc-800 hover:bg-white hover:text-black transition-colors cursor-pointer pb-4 duration-200"
-                >
-                </Link>
-                <Link href="#hero" className="text-white p-4 font-bold rounded-full hover:bg-white hover:text-black transition-colors">
-                    About
-                </Link>
-                <Link href="/auth/" className="text-white p-4 font-bold rounded-full hover:bg-white hover:text-black transition-colors">
-                    Add Your School
-                </Link>
-                <Link href="/auth/login" className="text-white p-4 font-bold rounded-full bg-zinc-800 hover:bg-white hover:text-black transition-colors">
-                    Login
-                </Link>
-
-            </div>
-
+  return (
+    <motion.div
+      className="z-100 fixed inset-x-0 bottom-0 hidden w-full items-center justify-center rounded-full p-7 sm:flex"
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="flex items-center justify-center rounded-full  border border-white/50 bg-[#14393D] p-1.5 backdrop-blur-md">
+        <div className="boder flex items-stretch p-2">
+          <Link
+            href="#hero"
+            style={{
+              backgroundImage:
+                "url(https://assets-global.website-files.com/5837424ae11409586f837994/624322b0c806026f5689d841_Group%2012569.svg)",
+              backgroundPosition: "50%",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "auto 22px",
+              paddingLeft: "38px",
+              paddingRight: "38px",
+            }}
+            className="cursor-pointer rounded-full bg-zinc-800 p-4 pb-4 font-bold text-white transition-colors duration-200 hover:bg-white hover:text-black"
+          ></Link>
+          <Link
+            href="/auth/"
+            className="rounded-full p-4 font-bold text-white transition-colors hover:bg-white hover:text-black"
+          >
+            Survey
+          </Link>
+          <Link
+            href="/auth/"
+            className="rounded-full p-4 font-bold text-white transition-colors hover:bg-white hover:text-black"
+          >
+            About
+          </Link>
+          <Link
+            href="https://lu.ma/hello_miami"
+            className="rounded-full p-4 font-bold text-white transition-colors hover:bg-white hover:text-black"
+          >
+            Calendar
+          </Link>
+          <Link
+            href="/auth/"
+            className="rounded-full p-4 font-bold text-white transition-colors hover:bg-white hover:text-black"
+          >
+            Join
+          </Link>
+          <Link
+            href="/auth/login"
+            className="rounded-full bg-zinc-800 p-4 font-bold text-white transition-colors hover:bg-white hover:text-black"
+          >
+            Login
+          </Link>
         </div>
-      </motion.div>
-    );
-  };
+      </div>
+    </motion.div>
+  );
+};
 
-export default FloatingNavbar
+export default FloatingNavbar;
