89868fa9a64f889fbd5f0682e9b44e90bb6f4a5d
Jose <jose@sirven.xyz>
2024-03-31 01:36:14 -0400

    rm old auth form

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -86,7 +86,7 @@ export default function HomePage() {
           </div>
         </div>
         <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          {/* <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
                 Join hello_miami
@@ -113,7 +113,7 @@ export default function HomePage() {
               </Link>
               .
             </p>
-          </div>
+          </div> */}
         </div>
         <video
           autoPlay
