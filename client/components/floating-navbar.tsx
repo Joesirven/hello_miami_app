0ce0f965e8f068d4aedef7ba1e2559d616c13739
Jose <jose@sirven.xyz>
2024-03-31 17:00:02 -0400

    added mobile for navbar

--- client/components/floating-navbar.tsx
+++ client/components/floating-navbar.tsx
@@ -1,64 +1,141 @@
 "use client";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { useState } from "react";
+import Image from "next/image";
 
 const FloatingNavbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <motion.div
-      className="z-100 fixed inset-x-0 bottom-0 w-full items-center justify-center rounded-full p-7 sm:flex"
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <div className="flex items-center justify-center rounded-full bg-[#14393D] p-1.5 backdrop-blur-md">
-        <div className="boder flex items-stretch p-2">
-          <Link
-            href="#hero"
-            style={{
-              backgroundImage:
-                "url(https://assets-global.website-files.com/5837424ae11409586f837994/624322b0c806026f5689d841_Group%2012569.svg)",
-              backgroundPosition: "50%",
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "auto 22px",
-              paddingLeft: "25px",
-              paddingRight: "25px",
-            }}
-            className="cursor-pointer rounded-full bg-zinc-800 p-2 pb-2 font-bold text-white transition-colors duration-200 hover:bg-white hover:text-black"
-          ></Link>
-          <Link
-            href="/auth/"
-            className="rounded-full p-2 font-bold text-white transition-colors hover:bg-white hover:text-black"
-          >
-            Survey
-          </Link>
-          <Link
-            href="/auth/"
-            className="rounded-full p-2 font-bold text-white transition-colors hover:bg-white hover:text-black"
-          >
-            About
-          </Link>
-          <Link
-            href="https://lu.ma/hello_miami"
-            className="rounded-full p-2 font-bold text-white transition-colors hover:bg-white hover:text-black"
-          >
-            Calendar
-          </Link>
-          <Link
-            href="/auth/"
-            className="rounded-full p-2 font-bold text-white transition-colors hover:bg-white hover:text-black"
-          >
-            Join
-          </Link>
-          <Link
-            href="/auth/login"
-            className="rounded-full bg-zinc-800 p-2 font-bold text-white transition-colors hover:bg-white hover:text-black"
-          >
-            Login
-          </Link>
+    <>
+      <motion.div
+        className="z-100 fixed inset-x-0 bottom-0 w-full items-center justify-center rounded-full p-7 sm:flex"
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="flex items-center justify-center rounded-full bg-[#14393D] p-1.5 backdrop-blur-md">
+          <div className="boder flex items-stretch p-2">
+            <Link
+              href="#hero"
+              style={{
+                backgroundImage:
+                  "url(https://assets-global.website-files.com/5837424ae11409586f837994/624322b0c806026f5689d841_Group%2012569.svg)",
+                backgroundPosition: "50%",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "auto 22px",
+                paddingLeft: "25px",
+                paddingRight: "25px",
+              }}
+              className="cursor-pointer rounded-full bg-zinc-800 p-2 pb-2 font-bold text-white transition-colors duration-200 hover:bg-white hover:text-black"
+            ></Link>
+            <Link
+              href="/auth/"
+              className="rounded-full p-2 font-bold text-white transition-colors hover:bg-white hover:text-black"
+            >
+              Survey
+            </Link>
+            <Link
+              href="/auth/"
+              className="rounded-full p-2 font-bold text-white transition-colors hover:bg-white hover:text-black"
+            >
+              About
+            </Link>
+            <Link
+              href="https://lu.ma/hello_miami"
+              className="rounded-full p-2 font-bold text-white transition-colors hover:bg-white hover:text-black"
+            >
+              Calendar
+            </Link>
+            <Link
+              href="/auth/"
+              className="rounded-full p-2 font-bold text-white transition-colors hover:bg-white hover:text-black"
+            >
+              Join
+            </Link>
+            <Link
+              href="/auth/login"
+              className="rounded-full bg-zinc-800 p-2 font-bold text-white transition-colors hover:bg-white hover:text-black"
+            >
+              Login
+            </Link>
+          </div>
         </div>
-      </div>
-    </motion.div>
-  );
+      </motion.div>
+
+      <motion.div
+        className="sm:hidden z-100 fixed bottom-5 right-5"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.5 }}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        <div className="rounded-full bg-blue-500 p-4 shadow-lg cursor-pointer">
+          <Image src="hmicon.png"
+            alt="icon"
+          />
+        </div>
+      </motion.div>
+      {isMenuOpen && (
+        <motion.div
+          className="fixed inset-0 bg-black/30 backdrop-blur-sm flex items-end sm:hidden"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <div className="w-full flex flex-col items-center justify-center bg-white rounded-t-lg p-4">
+            <div className="boder flex items-stretch p-2">
+              <Link
+                href="#hero"
+                style={{
+                  backgroundImage:
+                    "url(https://assets-global.website-files.com/5837424ae11409586f837994/624322b0c806026f5689d841_Group%2012569.svg)",
+                  backgroundPosition: "50%",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "auto 22px",
+                  paddingLeft: "25px",
+                  paddingRight: "25px",
+                }}
+                className="cursor-pointer rounded-full bg-zinc-800 p-2 pb-2 font-bold text-white transition-colors duration-200 hover:bg-white hover:text-black"
+              ></Link>
+              <Link
+                href="/auth/"
+                className="rounded-full p-2 font-bold text-white transition-colors hover:bg-white hover:text-black"
+              >
+                Survey
+              </Link>
+              <Link
+                href="/auth/"
+                className="rounded-full p-2 font-bold text-white transition-colors hover:bg-white hover:text-black"
+              >
+                About
+              </Link>
+              <Link
+                href="https://lu.ma/hello_miami"
+                className="rounded-full p-2 font-bold text-white transition-colors hover:bg-white hover:text-black"
+              >
+                Calendar
+              </Link>
+              <Link
+                href="/auth/"
+                className="rounded-full p-2 font-bold text-white transition-colors hover:bg-white hover:text-black"
+              >
+                Join
+              </Link>
+              <Link
+                href="/auth/login"
+                className="rounded-full bg-zinc-800 p-2 font-bold text-white transition-colors hover:bg-white hover:text-black"
+              >
+                Login
+              </Link>
+            </div>
+          </div>
+        </motion.div>
+      )};
+
+      );
+    </>
 };
 
 export default FloatingNavbar;
