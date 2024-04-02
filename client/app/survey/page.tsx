8a919376ddc8d0da7f09a17f366eaa8d3302d576
Jose <jose@sirven.xyz>
2024-04-02 17:45:04 -0400

    update survey typeform script

--- client/app/survey/page.tsx
+++ client/app/survey/page.tsx
@@ -1,11 +1,10 @@
+import Script from "next/script";
+
 function SurveyPage() {
   return (
     <div>
-      <h1>The hello_miami 2024 Engineers Survey</h1>
-      <div>
-        <div data-tf-live="01HTBV8V6EPW3VFPX06KH1WH8P"></div>
-        <script src="//embed.typeform.com/next/embed.js"></script>
-      </div>
+      <div data-tf-live="01HTBV8V6EPW3VFPX06KH1WH8P"></div>
+      <Script src="//embed.typeform.com/next/embed.js"></Script>
     </div>
   );
 }
