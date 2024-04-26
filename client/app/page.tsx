fd35b25a423f43660c198c47aad917f07f5b138e
Jose <jose@sirven.xyz>
2024-04-26 16:00:34 -0400

    test message for landing page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -19,7 +19,16 @@ export default function HomePage() {
           className="absolute left-0 top-0 z-0 h-full w-full object-cover"
           src="/background_video.mp4"
         />
-        <div className="absolute right-32 bg-[#14393D] opacity-80 w-[415px] h-[645px] rounded-md top-0 z-0"></div>
+        <div className="absolute right-32 bg-[#14393D] opacity-80 w-[415px] h-[645px] rounded-md top-0 z-0">
+          <div className="flex flex-col items-center justify-center h-full">
+            <h1 className="text-white text-4xl font-bold">Hello, Miami!</h1>
+            <p className="text-white text-lg mt-4">
+              We are a community of engineers, designers, and entrepreneurs
+              based in Miami, FL. We host events, workshops, and meetups to
+              connect and empower our members.
+            </p>
+          </div>
+        </div>
         <div className="absolute inset-x-0 bottom-0 pb-40">
           <PartnerTicker />
         </div>
---
+++ client/components/home-banner.tsx
@@ -0,0 +1,5 @@
+function HomeBanner() {
+  <div className="absolute right-32 bg-[#14393D] opacity-80 w-[415px] h-[645px] rounded-md top-0 z-0"></div>;
+}
+
+export default HomeBanner;
