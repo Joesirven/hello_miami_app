3405dae6aba57786c0b77e77ff00988bf5a32db7
Jose <jose@sirven.xyz>
2024-05-06 00:23:33 -0400

    aded types to providers

--- client/app/providers.tsx
+++ client/app/providers.tsx
@@ -2,12 +2,17 @@
 "use client";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
+import React, { ReactNode } from "react";
+
+interface CSPostHogProviderProps {
+  children: ReactNode;
+}
 
 if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   });
 }
-export function CSPostHogProvider({ children }) {
+export function CSPostHogProvider({ children }: CSPostHogProviderProps) {
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
