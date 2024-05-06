d260d2df0d90d3ee98ec42a431eafaa80f72f049
Jose <jose@sirven.xyz>
2024-05-06 00:14:23 -0400

    provider file and added posthot to layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@styles/globals.css";
 import FloatingNavbar from "@/components/floating-navbar";
+import { CSPostHogProvider } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <FloatingNavbar />
-      <body className={inter.className}>{children}</body>
+      <CSPostHogProvider>
+        <FloatingNavbar />
+        <body className={inter.className}>{children}</body>
+      </CSPostHogProvider>
     </html>
   );
 }
